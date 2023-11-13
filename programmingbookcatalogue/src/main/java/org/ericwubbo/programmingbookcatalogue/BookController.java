9ddb0203561c982677afd797e201cd323a3871b4
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2023-11-13 20:44:32 +0100

    Now with paging and sorting

--- programmingbookcatalogue/src/main/java/org/ericwubbo/programmingbookcatalogue/BookController.java
+++ programmingbookcatalogue/src/main/java/org/ericwubbo/programmingbookcatalogue/BookController.java
@@ -1,8 +1,12 @@
 package org.ericwubbo.programmingbookcatalogue;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("api/v1/books")
@@ -15,8 +19,15 @@ public class BookController {
     }
 
     @GetMapping
-    public Iterable<Book> getAll() {
-        return bookRepository.findAll();
+    public Iterable<Book> getAll(Pageable pageable) { // beware of correct import!
+        return bookRepository.findAll(pageable); // http://localhost:8080/api/v1/books?page=0&size=10&sort=title,desc
+    }
+
+    @GetMapping("limited")
+    public Iterable<Book> getAllLimited(Pageable pageable) {
+        return bookRepository.findAll(PageRequest.of(pageable.getPageNumber(), Math.min(pageable.getPageSize(), 3),
+                pageable.getSortOr(Sort.by(new Sort.Order(Sort.Direction.ASC, "title")))));
+                //Sort.by(new Sort.Order(Sort.Direction.ASC, "title")))); // http://localhost:8080/api/v1/books?page=0&size=10&sort=title,desc
     }
 
 }
